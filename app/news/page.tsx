611dcd91305c6005ff515de37426e425683aacd8
Juvenile Tubal <juveniletubal00@gmail.com>
2025-08-17 22:57:00 +0800

    3rd commit

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const NewsPage = () => {
-  return <div>NewsPage</div>;
+  return (
+    <div className="flex items-center justify-center mt-10">
+      <h1 data-aos="flip-left" className="text-3xl font-bold text-gray-700">
+        HAKDOG
+      </h1>
+    </div>
+  );
 };
 
 export default NewsPage;
